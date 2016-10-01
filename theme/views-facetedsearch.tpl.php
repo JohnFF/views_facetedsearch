f773c279634cbcb4a71215a4e6213979fb62537d
John Kirk <jkirk@ginkgostreet.com>
2016-10-01 18:23:17 +0100

    Added template container options. Changed some labels for clarity.

--- theme/views-facetedsearch.tpl.php
+++ theme/views-facetedsearch.tpl.php
@@ -1,9 +1,5 @@
-<table id='view_facetedsearch_facets'><tr></tr></table>
-<table>
-      <td><div class=facettotalcount></div></td>
-      <td><div class=deselectstartover>Deselect all filters</div></td>
-      <td><div class=orderby></div></td>
-</table>
+<?php print $views_facetedsearch_facetpanel ?>
+<?php print $views_facetedsearch_controlpanel ?>
 <?php print $views_facetedsearch_resultscontainer; ?>
 <script type="text/javascript">
   jQuery(function(){
@@ -12,15 +8,15 @@
       items              : views_facetedsearch_resultitems,
       facets             : { <?php print $views_facetedsearch_facets; ?> },
       resultSelector     : '#view_facetedsearch_results',
-      facetSelector      : '#view_facetedsearch_facets',
+      facetSelector      : '#view_facetedsearch_facetpanel',
       facetContainer     : '<?php print $views_facetedsearch_facetcontainer ?>',
-      facetListContainer : '<?php print $views_facetedsearch_facetlistcontainer ?>',
+      facetListContainer : '<?php print $views_facetedsearch_facetlist ?>',
       resultTemplate     : item_template,
       enablePagination   : <?php print $view->style_options['views_facetedsearch_enable_pagination'] ? 'true' : 'false' ?>,
       paginationCount    : <?php print $view->style_options['views_facetedsearch_pagination_count']  ? 
                                        $view->style_options['views_facetedsearch_pagination_count'] : '0' ?>,
       resultsHeader      : '<?php print $view->style_options['views_facetedsearch_resultsheader'] ?>',
-      bottomContainer    : '<table><tr class=bottomline></tr></table>',
+      bottomContainer    : '',
       orderByTemplate    : '<td class=orderby><span class="orderby-title">Sort by: </span><ul><% _.each(options, function(value, key) { %>'+
                            '<li class=orderbyitem id=orderby_<%= key %>>'+
                            '<%= value %> </li> <% }); %></ul></td>',
