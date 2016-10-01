e41abcd9edb0df549476f347d7e66f12288d5a47
John Kirk <john@civifirst.com>
2016-10-01 01:01:17 +0000

    Corrected an error by changing the js library.

--- theme/views-facetedsearch.tpl.php
+++ theme/views-facetedsearch.tpl.php
@@ -1,18 +1,26 @@
-<div id='view_facetedsearch_facets'></div>
+<table id='view_facetedsearch_facets'><tr ></tr></table>
 <?php print $views_facetedsearch_resultscontainer; ?>
 <script type="text/javascript">
   jQuery(function(){
     var item_template = '<?php print $views_facetedsearch_itemtemplate; ?>';
     settings = {
-      items            : views_facetedsearch_resultitems,
-      facets           : { <?php print $views_facetedsearch_facets; ?> },
-      resultSelector   : '#view_facetedsearch_results',
-      facetSelector    : '#view_facetedsearch_facets',
-      resultTemplate   : item_template,
-      enablePagination : <?php print $view->style_options['views_facetedsearch_enable_pagination'] ? 'true' : 'false' ?>,
-      paginationCount  : <?php print $view->style_options['views_facetedsearch_pagination_count']  ? 
+      items              : views_facetedsearch_resultitems,
+      facets             : { <?php print $views_facetedsearch_facets; ?> },
+      resultSelector     : '#view_facetedsearch_results',
+      facetSelector      : '#view_facetedsearch_facets',
+      facetContainer     : '<?php print $views_facetedsearch_facetcontainer ?>',
+      facetListContainer : '<?php print $views_facetedsearch_facetlistcontainer ?>',
+      resultTemplate     : item_template,
+      enablePagination   : <?php print $view->style_options['views_facetedsearch_enable_pagination'] ? 'true' : 'false' ?>,
+      paginationCount    : <?php print $view->style_options['views_facetedsearch_pagination_count']  ? 
                                        $view->style_options['views_facetedsearch_pagination_count'] : '0' ?>,
-      orderByOptions   : {<?php print $views_facetedsearch_facetorders ?>}
+      bottomContainer    : '<table><tr class=bottomline></tr></table>',
+      orderByTemplate    : '<td class=orderby><span class="orderby-title">Sort by: </span><ul><% _.each(options, function(value, key) { %>'+
+                           '<li class=orderbyitem id=orderby_<%= key %>>'+
+                           '<%= value %> </li> <% }); %></ul></td>',
+      countTemplate      : '<td class=facettotalcount><%= count %> Results</td>',
+      deselectTemplate   : '<td class=deselectstartover>Deselect all filters</td>',
+      orderByOptions     : { <?php print $views_facetedsearch_facetorders ?> }
     }
 
     // use them!
@@ -34,7 +42,6 @@
       $attributeIndex = 0;
       print '{';
       foreach ($eachResult as $attributeKey => $attribute){
-        $attribute = $attribute ? $attribute : "NONE";
         print '"' . addslashes($attributeKey) . '" : "' . addslashes(str_replace(array("\r", "\n"), '', $attribute)) . '"';
         $attributeIndex++;
         if ($attributeIndex != $attributeCount){
